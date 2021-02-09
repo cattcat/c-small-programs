049381d3bd7a0a4b56f5c48d6697a0951f18bd91
cattcat <78782049+cattcat@users.noreply.github.com>
2021-02-09 14:14:03 +0800

    Add files via upload

---
+++ 9x9.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 1; x <= 9; x++)
+            {
+                for (int y = 1; y <= 9; y++)
+                {
+                    Console.Write(x + "*" + y + "=" + (x * y)+"\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
